579862ec29f55131cc3d8dd64dcfa896c9b07de5
XzByte <suryapr009@students.amikom.ac.id>
2024-02-24 22:17:45 +0700

    Add import statement for idord module

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ print("""
       This is just the prototype of scanner, only scanner""")
 import sys
 sys.path.append('ScriptScan/')
+sys.path.append('ScriptScan/idord/Wrapper/')
 sys.path.append('Remed/')
 from ScriptScan import ScanOldSw
-
+from ScriptScan import idord 
 if __name__ == "__main__":
     ScanOldSw.scan_old_software.check_for_updates()
     
