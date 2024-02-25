432c5abf189491db4b31632dc2ec246b3f41eeea
XzByte <suryapr009@students.amikom.ac.id>
2024-02-25 14:12:04 +0700

    The commit modifies the subproject commit hash from `6280a72a20c6e755c1f28bdd233155c41f05c775` to `6280a72a20c6e755c1f28bdd233155c41f05c775-dirty`.

--- main.py
+++ main.py
@@ -2,12 +2,32 @@ print("""
       This is just the prototype of scanner, only scanner""")
 import sys
 sys.path.append('ScriptScan/')
-sys.path.append('ScriptScan/idord/Wrapper/')
 sys.path.append('Remed/')
 
-from ScriptScan import ScanOldSw
-from ScriptScan import idord
-if __name__ == "__main__":
-    ScanOldSw.scan_old_software.check_for_updates()
-    idord()
+
+print("""
+            
+░██████╗░█████╗░░░░░░░░█████╗░██████╗░███████╗░█████╗░
+██╔════╝██╔══██╗░░░░░░██╔══██╗██╔══██╗██╔════╝██╔══██╗
+╚█████╗░██║░░██║█████╗███████║██████╔╝█████╗░░╚═╝███╔╝
+░╚═══██╗██║░░██║╚════╝██╔══██║██╔══██╗██╔══╝░░░░░╚══╝░
+██████╔╝╚█████╔╝░░░░░░██║░░██║██║░░██║███████╗░░░██╗░░
+╚═════╝░░╚════╝░░░░░░░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝░░░╚═╝░░""")
+print('''Menu:
+      1. Scan for old software
+      2. Check IDOR vulnerability
+                
+      Your answer: ''')
+opt = input(str(">>> "))
+
+if opt == 1:
+      from ScriptScan import ScanOldSw
+      print("Scanning for old software...")
+      ScanOldSw.scan_old_software.check_for_updates()
+elif opt == 2:
+      from ScriptScan.IDORD.Wrapper import IDORD as idord
+      print("Checking for IDOR...")
+      instance = idord()
+# ScanOldSw.scan_old_software.check_for_updates()
+# instance = idord()
     
